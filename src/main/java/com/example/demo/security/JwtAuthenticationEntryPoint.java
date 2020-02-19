f66e9b648d41e76e2c5f0873325d958893a93f3f
arunkumarshahi@gmail.com <arunkumarshahi@gmail.com>
2020-02-19 23:39:31 +0530

    added jwt token and authentication

--- src/main/java/com/example/demo/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/demo/security/JwtAuthenticationEntryPoint.java
@@ -13,6 +13,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Request is Unauthorized." +
+                " Please provide valid JWT token.");
     }
 }
